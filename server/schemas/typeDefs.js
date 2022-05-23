8481e482add9e536d0a0bcd3de43f97a12b9fd16
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-05-23 18:09:38 +0800

    queries, mutations and utils from client side and server side completed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,30 +1,29 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    password: String
+    reviews: [Review]
+  }
 
-type User {
-  _id: ID!
-  username: String
-  email: String
-  password: String
-  reviews: [Review]
-}
+  type Review {
+    _id: ID!
+    latitude: Float
+    longitude: Float
+    reviewSection: [reviewSection]!
+  }
 
-type Review {
-  _id: ID!
-  latitude: Float
-  longitude: Float
-  reviewContent:[reviewContent]!
-}
-
-type reviewContent {
-  _id: ID!
-  title: String
-  text: String
-  reviewAuthor: String
-  stars: Int
-  createdAt: String
-}
+  type reviewSection {
+    _id: ID!
+    title: String
+    content: String
+    reviewAuthor: String
+    stars: Int
+    createdAt: String
+  }
 
   type Auth {
     token: ID!
@@ -32,20 +31,19 @@ type reviewContent {
   }
 
   type Query {
-    users: [User]
+    users: [User!]
     user(userId: ID!): User
-    me: User
     reviews(username: String): [Review]
     review(reviewId: ID!): Review
+    me: User
   }
 
   type Mutation {
-    addUser:(username: String!, email: String!, password: String!): Auth
+    addUser:(username: String!, email: String!, password: String!): Auth 
     login(email: String!, password: String!): Auth
-    addReview: [Review]
+    addReview(reviewId: String): Review
     deleteUser(userId: ID!): User
     deleteReview(reviewId: ID!): Review
-
   }
 `;
 
