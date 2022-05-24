7dba8b2c586a9ba352f79a47fe451bb3da9d32ee
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-05-24 20:57:52 +0800

    sign up end point working

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
+
 const Signup = () => {
   const [formState, setFormState] = useState({
     name: "",
     email: "",
     password: "",
   });
+
   const [addUser, { error, data }] = useMutation(ADD_USER);
   // update state based on form input changes
   const handleChange = (event) => {
@@ -18,6 +20,7 @@ const Signup = () => {
       [name]: value,
     });
   };
+
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -26,7 +29,7 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState },
       });
-      Auth.login(data.addProfile.token);
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
