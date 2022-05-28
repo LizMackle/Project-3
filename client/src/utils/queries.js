65814948bda4625431cfc00b357111f31e4b1218
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-05-28 19:28:28 +0800

    reverse changes

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
-  query getUser(userId: ID!) {
+  query getUser($userId: ID!) {
     user(userId: $userId) {
       _id
       username
@@ -25,20 +25,20 @@ export const QUERY_REVIEWS = gql`
   }
 `;
 
-export const QUERY_SINGLE_REVIEW = gql`
-  query getSingleReview($reviewId: ID!) {
-    review(reviewId: $reviewId) {
-      _id
-      latitude
-      longitude
-      title
-      content
-      reviewAuthorId
-      stars
-      createdAt
-    }
-  }
-`;
+// export const QUERY_SINGLE_REVIEW = gql`
+//   query getSingleReview($reviewId: ID!) {
+//     review(reviewId: $reviewId) {
+//       _id
+//       latitude
+//       longitude
+//       title
+//       content
+//       reviewAuthorId
+//       stars
+//       createdAt
+//     }
+//   }
+// `;
 
 export const QUERY_ME = gql`
   query me {
